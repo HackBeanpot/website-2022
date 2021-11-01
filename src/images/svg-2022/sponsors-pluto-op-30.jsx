fb4399503a837d6477bd8d34f27e1a86b163e2d7
jessicasu77 <jessica.su0807@gmail.com>
2021-11-01 18:31:16 -0400

    added all svgs

---
+++ src/images/svg-2022/sponsors-pluto-op-30.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const SponsorsPlutoNotFaded = () => (
+
+<svg width="112" height="114" viewBox="0 0 112 114" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_451:2073)">
+<path d="M54.21 113.29C41.5609 113.524 29.2441 109.233 19.4791 101.19C9.71402 93.1464 3.14292 81.8797 0.949959 69.42C-1.99004 52.24 2.04994 36.86 12.4299 23.16C22.0599 10.45 35 2.54996 50.78 0.539962C70.26 -1.94004 86.95 4.17996 99.72 19.54C109.32 31.09 113.02 44.68 111.72 59.38C109.31 85.78 89.72 107.16 64.83 112.21C61.3405 112.96 57.7789 113.322 54.21 113.29Z" fill="#70E0A3"/>
+<path d="M54.21 113.29C41.5609 113.524 29.2441 109.233 19.4791 101.19C9.71402 93.1464 3.14292 81.8797 0.949959 69.42C-1.99004 52.24 2.04994 36.86 12.4299 23.16C22.0599 10.45 35 2.54996 50.78 0.539962C70.26 -1.94004 86.95 4.17996 99.72 19.54C109.32 31.09 113.02 44.68 111.72 59.38C109.31 85.78 89.72 107.16 64.83 112.21C61.3405 112.96 57.7789 113.322 54.21 113.29Z" fill="url(#paint0_radial_451:2073)"/>
+<path d="M54.21 113.29C41.5609 113.524 29.2441 109.233 19.4791 101.19C9.71402 93.1464 3.14292 81.8797 0.949959 69.42C-1.99004 52.24 2.04994 36.86 12.4299 23.16C22.0599 10.45 35 2.54996 50.78 0.539962C70.26 -1.94004 86.95 4.17996 99.72 19.54C109.32 31.09 113.02 44.68 111.72 59.38C109.31 85.78 89.72 107.16 64.83 112.21C61.3405 112.96 57.7789 113.322 54.21 113.29Z" fill="url(#paint1_radial_451:2073)" fill-opacity="0.6"/>
+<path d="M54.21 113.29C41.5609 113.524 29.2441 109.233 19.4791 101.19C9.71402 93.1464 3.14292 81.8797 0.949959 69.42C-1.99004 52.24 2.04994 36.86 12.4299 23.16C22.0599 10.45 35 2.54996 50.78 0.539962C70.26 -1.94004 86.95 4.17996 99.72 19.54C109.32 31.09 113.02 44.68 111.72 59.38C109.31 85.78 89.72 107.16 64.83 112.21C61.3405 112.96 57.7789 113.322 54.21 113.29Z" fill="url(#paint2_radial_451:2073)"/>
+</g>
+<defs>
+<radialGradient id="paint0_radial_451:2073" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(41.5 56.9999) rotate(111.289) scale(114.3 112.951)">
+<stop stop-color="#85790C"/>
+<stop offset="1" stop-color="#85790C" stop-opacity="0"/>
+</radialGradient>
+<radialGradient id="paint1_radial_451:2073" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(32.5 56.9999) rotate(105.588) scale(120.948 119.521)">
+<stop stop-color="#414141"/>
+<stop offset="1" stop-opacity="0"/>
+</radialGradient>
+<radialGradient id="paint2_radial_451:2073" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(80.5 35.9999) rotate(90) scale(25.5 25.199)">
+<stop stop-color="#E0F36D" stop-opacity="0.55"/>
+<stop offset="1" stop-color="#A0B656" stop-opacity="0.06"/>
+</radialGradient>
+<clipPath id="clip0_451:2073">
+<rect width="111.93" height="113.3" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+);
+
+export default SponsorsPlutoNotFaded;
