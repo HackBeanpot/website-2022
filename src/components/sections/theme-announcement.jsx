eba716ecc4f3f65d9716a938fe4ff8ec1d138bb8
Judy <judy.zhang@toasttab.com>
2021-11-05 18:15:24 -0400

    added header and description

--- src/components/sections/theme-announcement.jsx
+++ src/components/sections/theme-announcement.jsx
@@ -1,42 +1,33 @@
 import React from 'react';
 
 import BlueTransition from 'images/svg/blue-transition.jsx';
-import Compass from 'images/svg/compass.jsx';
-import SunsetScene from 'images/svg/sunset-scene.jsx';
 
 const ThemeAnnouncement = () => (
-    <section className="theme-announcement">
-        <div className="theme__transition">
-            <BlueTransition />
+  <section className="theme-announcement">
+    <div className="container">
+      <div className="theme__content-wrapper">
+        <div className="theme__content">
+          <a className="theme__header">Blast off!</a>
+          <h2 className="theme__space-jam">
+            Explore the stars with us at <br /> our 10th annual event!
+          </h2>
+          <p className="theme__description">
+            HackBeanpot 2022 is gonna be out of this world! Join our community
+            of astronaut trainees* for a weekend of exploration, collaboration,
+            and fun! (Oh, and did we mention free food and swag?)
+          </p>
+          <p className="theme__description">
+            Hackers can expect to put their resourcefulness to the test, while
+            sharing stories, and learning new skills from peers. So whether
+            you're a seasoned hackathon-goer, an
+            ‘I-have-never-written-a-line-of-code’ beginner, or someone
+            in-between, we’re excited for you to embark on this adventure with
+            us!
+          </p>
         </div>
-        <div className="container">
-            <div className="theme__content-wrapper">
-                <div className="theme__content">
-                    <h2 className="theme__camp">Our theme this year is Camping!</h2>
-                    <p>
-                        At HackBeanpot 2021, we aim to create a welcoming “campsite”
-                        by emphasizing our focus on exploration, creativity, and respect
-                        for the world around us! Camping represents community,
-                        sustainability, and the goal to help each other thrive
-                        given a set of tools.
-                    </p>
-                    <p>
-                        Hackers can expect to put their resourcefulness to the test,
-                        while sharing stories, and learning new skills from peers.
-                        So whether you're a seasoned hackathon-goer, an
-                        ‘I-have-never-written-a-line-of-code’ beginner, or someone
-                        in between, we’re excited for you to embark on this adventure with us!
-                    </p>
-                </div>
-                <div className="theme__logo">
-                    <Compass />
-                </div>
-            </div>
-        </div>
-        <div className="theme__sunset-scene">
-            <SunsetScene />
-        </div>
-    </section>
+      </div>
+    </div>
+  </section>
 );
 
 export default ThemeAnnouncement;
