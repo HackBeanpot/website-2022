afd4c8fa0f66b02b0e6fbad0710ed6719bff4652
Judy <judy.zhang@toasttab.com>
2021-11-07 14:58:32 -0500

    styled each individual item

--- src/components/sections/about.jsx
+++ src/components/sections/about.jsx
@@ -1,7 +1,27 @@
 import React, {useState} from 'react';
 import AboutItem from '../about/about-item';
 import Growth from '../../images/growth.jpg';
+import Exploration from '../../images/exploration.jpg';
+import Community from '../../images/community.jpg';
 const About = props => {
+
+  const aboutInfo = [
+    {
+      img: Community,
+      title: 'Community',
+      description: 'Connect with fellow students and our partners in the Boston tech community. Build connections that will last for lightyears!'
+    },
+    {
+      img: Exploration,
+      title: 'Exploration',
+      description: 'Discover new ideas and technologies (and maybe even galaxies?!) with the help of experienced mentors.'
+    },
+    {
+      img: Growth, 
+      title: 'Growth',
+      description: 'Expand beyond your horizons and grow your current skill set in a safe and supportive environment. '
+    }
+  ]
 
   return (
     <div id="about" className="about__container">
@@ -9,7 +29,8 @@ const About = props => {
         HackBeanpot is about...
       </h2>
       <div className='about-content'>
-        <AboutItem img={Growth} title="Growth" description="akjsdhflkjashdfkl"/>
+        {aboutInfo.map(info => <AboutItem img={info.img} title={info.title} description={info.description}/>)}
+
       </div>
     </div>
   )
