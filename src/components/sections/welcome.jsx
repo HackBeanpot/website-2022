63937c4c980453a832f620ca48df177791716061
meebs <alex@takayamasan.com>
2021-11-14 13:17:22 -0500

    February XX-XX -> in February

--- src/components/sections/welcome.jsx
+++ src/components/sections/welcome.jsx
@@ -12,7 +12,7 @@ const Welcome = () => (
       <h1 className="header__title">HackBeanpot 2022</h1>
       <SpaceJamTitle />
       <h4 className="header__description">
-        Coming to a galaxy near you February XX-XX
+        Coming to a galaxy near you in February
       </h4>
       <PrimaryCta
         link="https://hackbeanpot.us10.list-manage.com/subscribe?u=a98050d47fdae2481521f0474&id=dccd8c8431"
