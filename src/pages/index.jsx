487a89ede6b2ca9862c5510bcc508c3f58251a06
Judy <judy.zhang@toasttab.com>
2021-11-22 18:16:10 -0500

    removed testimonials

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,7 +16,7 @@ const App = () => {
         <ThemeAnnouncement />
         <About />
         <CovidPrecautions />
-        <Testimonials />
+        {/* <Testimonials /> */}
         {/*
          <FAQ />
         <Sponsors2021 /> */}
