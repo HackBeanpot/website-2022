24098e0e9107efa5f9bb36104f689ef4245661e2
Jamie Lin <jamie.miki.lin@gmail.com>
2021-11-22 19:03:09 -0500

    added box, need to format

--- src/components/sections/testimonials.jsx
+++ src/components/sections/testimonials.jsx
@@ -11,25 +11,25 @@ const testimonialInfo = [
     author: 'Ben Henderson',
     description:
       ' It was my first Hackathon, and I had a really great time - if you\'ve never done one before, this is a great choice. Really good environment for people who want to learn something new, meet new people, and/or challenge themselves. It\'s basically what you make of it, and there was plenty of support available if you have no idea what you\'re doing.'
-  },
-  {
-    img: AlbertZhang,
-    author: 'Albert Zhang',
-    description:
-      'This was my second hackathon ever and it was a lot of fun! The mentors were super helpful, and I was able to play around with Tensorflow, which is something I\'ve never done before.'
-  },
-  {
-    img: OnotinaImoudu,
-    author: 'Onotina Imoudu',
-    description:
-      'This was my first hackathon and I am definitely attending next year!!! Hackbeanpot is a must attend for anyone who has a passion or wants to get their feet wet in design or coding. And we had a zoom-ba session!! A zumba class on zoom, it was really fun, I was sweaty and out of breath.'
-  },
-  {
-    img: Anonymous,
-    author: 'Anonymous',
-    description:
-      'HackBeanpot is an awesome way to get introduced to the world of Hackathons, and everyone here is just excited for you to learn.'
   }
+//   {
+//     img: AlbertZhang,
+//     author: 'Albert Zhang',
+//     description:
+//       'This was my second hackathon ever and it was a lot of fun! The mentors were super helpful, and I was able to play around with Tensorflow, which is something I\'ve never done before.'
+//   },
+//   {
+//     img: OnotinaImoudu,
+//     author: 'Onotina Imoudu',
+//     description:
+//       'This was my first hackathon and I am definitely attending next year!!! Hackbeanpot is a must attend for anyone who has a passion or wants to get their feet wet in design or coding. And we had a zoom-ba session!! A zumba class on zoom, it was really fun, I was sweaty and out of breath.'
+//   },
+//   {
+//     img: Anonymous,
+//     author: 'Anonymous',
+//     description:
+//       'HackBeanpot is an awesome way to get introduced to the world of Hackathons, and everyone here is just excited for you to learn.'
+//   }
 ];
 
 const Testimonials = props => {
@@ -42,21 +42,10 @@ const Testimonials = props => {
     />
   ));
   return (
-    <div id="about" className="testimonials">
+    <div id="testimonials" className="testimonials__container">
           <h2 className="testimonials__title">Testimonials</h2>
-      <div className="testimonials__desktop">{testimonialChildren}</div>
-      {/* <div className="about__mobile">
-        <Carousel itemsToShow={1} itemPadding={[0]}>
-          {testimonialInfo.map(info => (
-            <TestimonialsItem
-              key={info.title}
-              img={info.img}
-              title={info.title}
-              description={info.description}
-            />
-          ))}
-        </Carousel>
-      </div> */}
+      <div className="testimonials__box">{testimonialChildren}</div>
+      // another div for checking out archive
     </div>
   );
 };
