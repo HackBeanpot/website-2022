42260c02721b0639920d80be008780f56de7ef0c
Judy <judy.zhang@toasttab.com>
2021-11-24 20:46:43 -0500

    checkpoint

--- src/components/sections/testimonials.jsx
+++ src/components/sections/testimonials.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TestimonialsItem from '../testimonials-items/testimonial-item';
 import BenHenderson from '../../images/ben-henderson.jpg';
 import AlbertZhang from '../../images/albert-zhang.jpg';
@@ -8,32 +8,34 @@ import TestimonialsMarsDim from '../../images/svg-2022/testimonials-mars-dim';
 import TestimonialsMercuryDim from '../../images/svg-2022/testimonials-mercury-dim';
 import TestimonialsNeptuneDim from '../../images/svg-2022/testimonials-neptune-dim';
 import TestimonialsVenusDim from '../../images/svg-2022/testimonials-venus-dim';
+import TestimonialsMercuryBright from '../../images/svg-2022/testimonials-mercury-bright';
 
 const testimonialInfo = [
   {
     img: BenHenderson,
     author: 'Ben Henderson',
     description:
-      ' It was my first Hackathon, and I had a really great time - if you\'ve never done one before, this is a great choice. Really good environment for people who want to learn something new, meet new people, and/or challenge themselves. It\'s basically what you make of it, and there was plenty of support available if you have no idea what you\'re doing.'
-  }
-//   {
-//     img: AlbertZhang,
-//     author: 'Albert Zhang',
-//     description:
-//       'This was my second hackathon ever and it was a lot of fun! The mentors were super helpful, and I was able to play around with Tensorflow, which is something I\'ve never done before.'
-//   },
-//   {
-//     img: OnotinaImoudu,
-//     author: 'Onotina Imoudu',
-//     description:
-//       'This was my first hackathon and I am definitely attending next year!!! Hackbeanpot is a must attend for anyone who has a passion or wants to get their feet wet in design or coding. And we had a zoom-ba session!! A zumba class on zoom, it was really fun, I was sweaty and out of breath.'
-//   },
-//   {
-//     img: Anonymous,
-//     author: 'Anonymous',
-//     description:
-//       'HackBeanpot is an awesome way to get introduced to the world of Hackathons, and everyone here is just excited for you to learn.'
-//   }
+      ' It was my first Hackathon, and I had a really great time - if you\'ve never done one before, this is a great choice. Really good environment for people who want to learn something new, meet new people, and/or challenge themselves. It\'s basically what you make of it, and there was plenty of support available if you have no idea what you\'re doing.',
+    index: 'mercury'
+  },
+   {
+     img: AlbertZhang,
+     author: 'Albert Zhang',
+     description:
+       'This was my second hackathon ever and it was a lot of fun! The mentors were super helpful, and I was able to play around with Tensorflow, which is something I\'ve never done before.'
+   },
+   {
+     img: OnotinaImoudu,
+     author: 'Onotina Imoudu',
+     description:
+       'This was my first hackathon and I am definitely attending next year!!! Hackbeanpot is a must attend for anyone who has a passion or wants to get their feet wet in design or coding. And we had a zoom-ba session!! A zumba class on zoom, it was really fun, I was sweaty and out of breath.'
+   },
+   {
+     img: Anonymous,
+     author: 'Anonymous',
+     description:
+       'HackBeanpot is an awesome way to get introduced to the world of Hackathons, and everyone here is just excited for you to learn.'
+   }
 ];
 
 const Testimonials = props => {
@@ -45,12 +47,25 @@ const Testimonials = props => {
       description={info.description}
     />
   ));
+
+  const [selected, updateSelected] = useState(testimonialInfo[0].index);
+
   return (
     <div id="testimonials" className="testimonials__container">
           <h2 className="testimonials__title">Testimonials</h2>
-      <div className="testimonials__box">{testimonialChildren}</div>
+      <div className="testimonials__box">
+      <TestimonialsItem
+      key={selected.author}
+      img={selected.img}
+      author={selected.author}
+      description={selected.description}
+    />
+        </div>
       <div className="planet_selection_container">
+        {console.log(selected.index)}
+        {selected === 'mercury' ? 
         <TestimonialsMercuryDim className="testimonials__planet"/>
+        : <TestimonialsMercuryBright className="testimonials__planet" />}
         <TestimonialsVenusDim className="testimonials__planet"/>
         <TestimonialsNeptuneDim className="testimonials__planet"/>
         <TestimonialsMarsDim className="testimonials__planet"/>
