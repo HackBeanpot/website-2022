80de8f24f212407bb8dfc1c799eeb9826879192d
jessicasu77 <jessica.su0807@gmail.com>
2021-11-29 19:03:43 -0500

    initial team section

--- src/components/sections/team.jsx
+++ src/components/sections/team.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import SecondaryCta from '../secondary-cta';
-
+import TeamPicture from '../../images/team-picture.jpeg'
 const team = () => (
-    
-    <div className="container">
-        <div className="theme__content-wrapper">
-            <div className="theme__content">
+    <div className="team__container container">
+        <div className="team__content-wrapper">
+            <div className="team__content">
                 <a className="theme__header">OUR TEAM</a>
-                <h2 className="theme__space-jam">
+                <h2 className="team__space-jam">
                     Meet this years mission crew behind HackBeanpot!
                 </h2>
-                <p className="theme__description">
+                <p className="team__description">
                     The HackBeanpot Core Team has been hard at work bringing HackBeanpot 2022 to a galaxy near you! We are a group of 22 driven students from universities in the Boston area who aim to make technical experience and knowledge accessible to anyone who’s interested.
                 </p>
                 <SecondaryCta link="mailto:team@hackbeanpot.com" textDescription="Contact us" />
+
             </div>
+            <img className='team__picture' src={TeamPicture} />
         </div>
     </div>
 );
