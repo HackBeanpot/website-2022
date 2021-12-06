6232cc8b1bdade836448600ac0d6292d150a4f3a
thejamielin <71473137+thejamielin@users.noreply.github.com>
2021-12-06 17:06:51 -0500

    Testimonials2 (#33)
    * change field name to type
    
    * got rid of extra field
    
    * finished abstracting the planet logic

--- src/components/sections/testimonials.jsx
+++ src/components/sections/testimonials.jsx
@@ -28,40 +28,32 @@ const testimonialInfo = [
     author: 'Ben Henderson',
     description:
       " It was my first Hackathon, and I had a really great time - if you've never done one before, this is a great choice. Really good environment for people who want to learn something new, meet new people, and/or challenge themselves. It's basically what you make of it, and there was plenty of support available if you have no idea what you're doing.",
-    index: TESTIMONIAL_PLANETS.MERCURY
+    type: TESTIMONIAL_PLANETS.MERCURY
   },
   {
     img: AlbertZhang,
     author: 'Albert Zhang',
     description:
       "This was my second hackathon ever and it was a lot of fun! The mentors were super helpful, and I was able to play around with Tensorflow, which is something I've never done before.",
-    index: TESTIMONIAL_PLANETS.VENUS
+    type: TESTIMONIAL_PLANETS.VENUS
   },
   {
     img: OnotinaImoudu,
     author: 'Onotina Imoudu',
     description:
       'This was my first hackathon and I am definitely attending next year!!! Hackbeanpot is a must attend for anyone who has a passion or wants to get their feet wet in design or coding. And we had a zoom-ba session!! A zumba class on zoom, it was really fun, I was sweaty and out of breath.',
-    index: TESTIMONIAL_PLANETS.NEPTUNE
+    type: TESTIMONIAL_PLANETS.NEPTUNE
   },
   {
     img: Anonymous,
     author: 'Anonymous',
     description:
       'HackBeanpot is an awesome way to get introduced to the world of Hackathons, and everyone here is just excited for you to learn.',
-    index: TESTIMONIAL_PLANETS.MARS
+    type: TESTIMONIAL_PLANETS.MARS
   }
 ];
 
-const Testimonials = props => {
-  const testimonialChildren = testimonialInfo.map(info => (
-    <TestimonialsItem
-      key={info.author}
-      img={info.img}
-      author={info.author}
-      description={info.description}
-    />
-  ));
+const Testimonials = () => {
 
   const [selected, setSelected] = useState(testimonialInfo[0]);
 
@@ -72,10 +64,8 @@ const Testimonials = props => {
         <TestimonialsItem item={selected} />
       </div>
       <div className="planet_selection_container">
-        {selected.index === TESTIMONIAL_PLANETS.MERCURY ? <TestimonialsMercuryBright className="testimonials__planet" /> : <TestimonialsMercuryDim className="testimonials__planet" setSelected={() => setSelected(testimonialInfo.find(curr => curr.index === TESTIMONIAL_PLANETS.MERCURY))}/> }
-        {selected.index === TESTIMONIAL_PLANETS.VENUS ? <TestimonialsVenusBright className="testimonials__planet" /> : <TestimonialsVenusDim className="testimonials__planet" setSelected={() => setSelected(testimonialInfo.find(curr => curr.index === TESTIMONIAL_PLANETS.VENUS))}/> }
-        {selected.index === TESTIMONIAL_PLANETS.NEPTUNE ? <TestimonialsNeptuneBright className="testimonials__planet" /> : <TestimonialsNeptuneDim className="testimonials__planet" setSelected={() => setSelected(testimonialInfo.find(curr => curr.index === TESTIMONIAL_PLANETS.NEPTUNE))}/>}
-        {selected.index === TESTIMONIAL_PLANETS.MARS ? <TestimonialsMarsBright className="testimonials__planet" /> : <TestimonialsMarsDim className="testimonials__planet" setSelected={() => setSelected(testimonialInfo.find(curr => curr.index === TESTIMONIAL_PLANETS.MARS))}/> }
+        {testimonialInfo.map(testimonial =>
+          <TestimonialsPlanet testimonial={testimonial} selected={selected.type === testimonial.type} setSelected={setSelected} />)}
       </div>
       <div className="archive__content">
         <p className="archive__description">
@@ -86,8 +76,44 @@ const Testimonials = props => {
         <PrimaryCta link="https://projects.hackbeanpot.com/" textDescription="Visit archive" />
       </div>
       <UranusOrbit />
-    </section>    
+    </section>
   );
 };
+
+const TestimonialsPlanet = ( props ) => {
+  const testimonial = props.testimonial
+  const selected = props.selected
+  const setSelected = props.setSelected
+  if (selected) {
+    const TestimonialsPlanetBright = getTestimonialsPlanetBright(testimonial.type)
+    return <TestimonialsPlanetBright className="testimonials__planet" />
+  }
+  else {
+    const TestimonialsPlanetDim = getTestimonialsPlanetDim(testimonial.type)
+    return <TestimonialsPlanetDim 
+    className="testimonials__planet" 
+    setSelected={() => setSelected(testimonial)} />
+  }
+}
+
+const getTestimonialsPlanetBright = ( type ) => {
+  switch( type ) {
+    case TESTIMONIAL_PLANETS.MARS: return TestimonialsMarsBright
+    case TESTIMONIAL_PLANETS.MERCURY: return TestimonialsMercuryBright 
+    case TESTIMONIAL_PLANETS.NEPTUNE: return TestimonialsNeptuneBright 
+    case TESTIMONIAL_PLANETS.VENUS: return TestimonialsVenusBright 
+    default: throw new Error("Unexpected type: " + type)
+  }
+}
+
+const getTestimonialsPlanetDim = ( type ) => {
+  switch( type ) {
+    case TESTIMONIAL_PLANETS.MARS: return TestimonialsMarsDim 
+    case TESTIMONIAL_PLANETS.MERCURY: return TestimonialsMercuryDim 
+    case TESTIMONIAL_PLANETS.NEPTUNE: return TestimonialsNeptuneDim 
+    case TESTIMONIAL_PLANETS.VENUS: return TestimonialsVenusDim 
+    default: throw new Error("Unexpected type: " + type)
+  }
+}
 
 export default Testimonials;
