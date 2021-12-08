4ff54ccd35e4d04a05cae663f5bfda7570348e21
Karen Li <li.kar@northeastern.edu>
2021-12-07 23:54:44 -0500

    condensed those two consts

--- src/components/primary-cta.jsx
+++ src/components/primary-cta.jsx
@@ -5,27 +5,25 @@ const PrimaryCta = ({ link, textDescription }) => {
     const [isClicked, setIsClicked] = useState(false);
     if (link == null) {
         const ctaText = isClicked ? "Copied to clipboard!" : textDescription;
-        const onClick = () => {
+        const onClick = async e => {
         setIsClicked(true);
         // second argument is in milliseconds, so 3 * 1000 = 3 seconds
         // after 3 seconds, setTimeout will call the function you provide
         setTimeout(() => setIsClicked(false), 3 * 1000);
+        e.preventDefault();
+
+        const el = document.createElement('input');
+        el.value = window.location.href;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+
         }
-
-        const clicked = async e => {
-            e.preventDefault();
-
-            const el = document.createElement('input');
-            el.value = window.location.href;
-            document.body.appendChild(el);
-            el.select();
-            document.execCommand('copy');
-            document.body.removeChild(el);
-        } 
 
         return (
             <div>
-                <button onClick={clicked, onClick} 
+                <button onClick={onClick} 
                     className = "launch-button"
                 >
                     {ctaText} <CtaRocketship />
