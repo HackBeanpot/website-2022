5eaa4128a886b29bcc10db1653a943a148184e1a
jessicasu77 <jessica.su0807@gmail.com>
2021-12-14 00:13:01 -0500

    abstracted smallTeamInfo into a list of dictionaries

--- src/components/sections/team.jsx
+++ src/components/sections/team.jsx
@@ -1,10 +1,37 @@
 import React, { useState } from 'react';
 import SecondaryCta from '../secondary-cta';
 import TeamPicture from '../../images/team-picture.jpeg'
+
+const smallTeamInfo = [
+    {
+        teamName: "Leadership",
+        teamPictures: []
+    },
+    {
+        teamName: "Design",
+        teamPictures: []
+    },
+    {
+        teamName: "Tech",
+        teamPictures: []
+    },
+    {
+        teamName: "Social/Outreach",
+        teamPictures: []
+    },
+    {
+        teamName: "Sponsorship",
+        teamPictures: []
+    }
+]
+
+
 const Team = () => {
-    const [smallTeamClicked, setSmallTeamClicked] = useState('Leadership')
+    const [smallTeamClicked, setSmallTeamClicked] = useState({
+        teamName: "Leadership",
+        teamPictures: []
+    })
     return (
-        // <div className="container">
         <div className="team__container">
             <div className="team__content-wrapper">
                 <div className="team__content">
@@ -15,13 +42,14 @@ const Team = () => {
                     <p className="team__description">
                         The HackBeanpot Core Team has been hard at work bringing HackBeanpot 2022 to a galaxy near you! We are a group of 22 driven students from universities in the Boston area who aim to make technical experience and knowledge accessible to anyone who’s interested.
                     </p>
-                    {/* <SecondaryCta textDescription="Leadership" /> */}
                 </div>
                 <img className='team__picture' src={TeamPicture} />
             </div>
             <div className="team__small-teams">
                 <div className="team__small-teams-buttons">
                     {RenderSmallTeamButtons(setSmallTeamClicked)}
+                </div>
+                <div className="team__small-team-clicked">
                     {RenderTeamClicked(smallTeamClicked)}
                 </div>
             </div>
@@ -31,11 +59,10 @@ const Team = () => {
 }
 
 const RenderSmallTeamButtons = (setSmallTeamClicked) => {
-    const allSmallTeams = ['Leadership', 'Design', 'Tech', 'Social/Outreach', 'Sponsorship']
     return (
-        allSmallTeams.map(smallTeam =>
+        smallTeamInfo.map(smallTeam =>
             <div onClick={() => setSmallTeamClicked(smallTeam)}>
-                <SecondaryCta textDescription={smallTeam} />
+                <SecondaryCta textDescription={smallTeam.teamName} />
             </div>
         ));
 
@@ -43,7 +70,7 @@ const RenderSmallTeamButtons = (setSmallTeamClicked) => {
 
 // TODO: change RenderTeamClicked to render images of team
 const RenderTeamClicked = (smallTeam) => {
-    return (<div>{smallTeam}</div>);
+    return (<div>{smallTeam.teamName}</div>);
 }
 
 export default Team;
