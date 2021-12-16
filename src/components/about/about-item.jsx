60d6c7a4e48fe91d382d0c6f9a0bc80b0d31b330
Judy <judy.zhang@toasttab.com>
2021-12-16 01:30:19 -0500

    added about hover and cursor pointer for testimonials planet

--- src/components/about/about-item.jsx
+++ src/components/about/about-item.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 
 const AboutItem = ({ img, title, description }) => {
+  const [hover, setHover] = useState(false);
   return (
     <>
       <div className="about-item-container">
-        <img className="about-item-img" src={img} />
+        <img
+          className={
+            hover ? 'about-item-img img-hover' : 'about-item-img img-normal'
+          }
+          src={img}
+          onMouseOver={() => setHover(true)}
+          onMouseOut={() => setHover(false)}
+        />
         <div className="about-item-text-container">
           <h2 className="about-item-title">{title}</h2>
           <p>{description}</p>
