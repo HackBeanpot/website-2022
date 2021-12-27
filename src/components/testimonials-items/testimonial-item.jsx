c80d60e6aa4c534221380fd84b86d31b8be6eecd
meebs <alex@takayamasan.com>
2021-12-27 01:05:11 -0800

    fix testimonials images

--- src/components/testimonials-items/testimonial-item.jsx
+++ src/components/testimonials-items/testimonial-item.jsx
@@ -4,7 +4,7 @@ const TestimonialsItem = ({ item }) => {
   return (
     <>
       <div className="testimonials-item-container">
-        <img className="testimonials-item-img" src={item.img} />
+        {item.img}
         <div className="testimonials-item-text-container">
           <p>{item.description}</p>
           <h2 className="testimonials-item-author">{item.author}</h2>
