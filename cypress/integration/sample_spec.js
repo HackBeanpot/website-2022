e7ab2eda32e8c266d36e2aee7cc1c84eba2b17ce
meebs <alex@takayamasan.com>
2021-12-29 16:30:10 -0800

    wait for images to load

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,8 +1,21 @@
 describe('Integration test with visual testing', function() {
-  it('Loads the main page', function() {
-    // Load the page or perform any other interactions with the app.
-    cy.visit('/');
+  beforeEach(() => {
+    // Needs to be called before cy.route
+    cy.server();
 
+    // Alias request
+    cy.route('GET', '*').as('showAll');
+    cy.visit('/');
+  });
+
+  it('Loads the main page', function() {
+    cy.contains('About').click();
+    cy.contains('COVID Safety').click();
+    cy.contains('FAQ').click();
+    cy.contains('Testimonials').click();
+    cy.contains('Team').click();
+
+    cy.wait('@showAll');
     // Take a snapshot for visual diffing
     cy.percySnapshot('Main page');
   });
