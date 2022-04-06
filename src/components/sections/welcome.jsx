095b407645803a8e8cc1489a61c861cd5af610bc
Alex Takayama <alex@takayamasan.com>
2022-04-06 00:38:59 -0400

    update copy with signup for mailing list

--- src/components/sections/welcome.jsx
+++ src/components/sections/welcome.jsx
@@ -4,15 +4,20 @@ import SpaceJamMars from '../../images/svg-2022/spacejam-mars.svg';
 import SpaceJamSaturnOrbit from '../../images/svg-2022/space-jam-saturn-orbit.svg';
 import SpaceJamEarth from '../../images/svg-2022/space-jam-earth.svg';
 import SpaceJamNeptune from '../../images/svg-2022/spacejam-neptune.svg';
+import PrimaryCta from '../primary-cta';
 
 const Welcome = () => (
   <div className="header__container">
     <div className="header__content">
-      <h1 className="header__title">HackBeanpot 2022</h1>
+      <h1 className="header__title">HackBeanpot 2023</h1>
       <img className='space-jam-title' src={SpaceJamTitle} alt="the text 'space jam' with a white halo surrounding it" />
       <h4 className="header__description">
-        Coming to you virtually February 11th-13th
+        Join our mailing list to stay updated on our future adventures!
       </h4>
+      <PrimaryCta
+        link="https://hackbeanpot.us10.list-manage.com/subscribe?u=a98050d47fdae2481521f0474&id=dccd8c8431"
+        textDescription="Sign up here"
+      />
     </div>
     <img className="space-jam-mars" src={SpaceJamMars} alt="the planet mars" />
     <img className="space-jam-saturn-orbit" src={SpaceJamSaturnOrbit} alt="the planet saturn" />
